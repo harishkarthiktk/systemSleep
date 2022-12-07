8c14909623bcec0594f04335a9e7322af2525fc0
harishkarthiktk <45687759+harishkarthiktk@users.noreply.github.com>
2022-12-08 01:06:49 +0530

    Update systemSleep.py
    corrected logging

--- systemSleep.py
+++ systemSleep.py
@@ -8,7 +8,7 @@ def sleepy():
 			tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec, tm_wday, tm_yday, tm_isdst = time.localtime()
 			current_datetime = f'[{tm_mday}-{tm_mon}-{tm_year}][{tm_hour}:{tm_min}:{tm_sec}]'
 
-			afile.write(f'{current_datetime}: putting system to sleep...')
+			afile.write(f'{current_datetime}: putting system to sleep...\n')
 
 			try:
 				os.system("Rundll32.exe Powrprof.dll,SetSuspendState Sleep")
